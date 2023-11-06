5261fc554b5f041408bed5a4d24d2970bd2073c0
ctorresm96 <carlostm320@gmail.com>
2023-11-05 23:35:02 -0500

    fix: BANCOM-00015 fix icon orders in landing component

--- src/app/features/landing/views/landing/landing.component.ts
+++ src/app/features/landing/views/landing/landing.component.ts
@@ -12,37 +12,37 @@ export class LandingComponent implements OnInit {
   ngOnInit(): void {
     this.features = [
       {
-        image: '/assets/svg/landing/icon-1.svg',
+        image: '/assets/svg/landing/icon-6.svg',
         title: 'Robust Workflow',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
       },
       {
-        image: '/assets/svg/landing/icon-2.svg',
+        image: '/assets/svg/landing/icon-1.svg',
         title: 'Flexibility',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
       },
       {
-        image: '/assets/svg/landing/icon-3.svg',
+        image: '/assets/svg/landing/icon-2.svg',
         title: 'User friendly',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
       },
       {
-        image: '/assets/svg/landing/icon-4.svg',
+        image: '/assets/svg/landing/icon-3.svg',
         title: 'Multiple layouts',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
       },
       {
-        image: '/assets/svg/landing/icon-5.svg',
+        image: '/assets/svg/landing/icon-4.svg',
         title: 'Better components',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
       },
       {
-        image: '/assets/svg/landing/icon-6.svg',
+        image: '/assets/svg/landing/icon-5.svg',
         title: 'Well organised',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
@@ -51,22 +51,22 @@ export class LandingComponent implements OnInit {
 
     this.achievements = [
       {
-        image: '/assets/svg/landing/icon-1.svg',
+        image: '/assets/svg/landing/icon-6.svg',
         title: '10,000+',
         description: 'Downloads per day',
       },
       {
-        image: '/assets/svg/landing/icon-2.svg',
+        image: '/assets/svg/landing/icon-3.svg',
         title: '2 Million',
         description: 'Users',
       },
       {
-        image: '/assets/svg/landing/icon-3.svg',
+        image: '/assets/svg/landing/icon-4.svg',
         title: '500+',
         description: 'Clients',
       },
       {
-        image: '/assets/svg/landing/icon-4.svg',
+        image: '/assets/svg/landing/icon-6.svg',
         title: '140',
         description: 'Countries',
       },
